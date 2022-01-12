ff8c021efad251789d54bd07b2237a686a0e2d34
Ethan <epigou@uwo.ca>
2022-01-12 17:53:19 -0500

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import React from 'react';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
     <h1> Hello World</h1>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    
     </div>
   );
 }
